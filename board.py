5c579e6f98e2822fa44332ce654c63f2e837d3da
SaumyaK <saumyakhati1@gmail.com>
2022-01-23 00:07:05 -0500

    Shift more board related methods from game.py --> board.py

--- board.py
+++ board.py
@@ -1,22 +1,48 @@
-# Define Board class that represents the n by n tic tac toe grid.
+# Class that represents an n by n tic tac toe grid.
 # A -1 represents a '-' (empty), 0 represents 'o' and 1 represents 'x'.
 class Board:
     VALUE_MAP = {-1:'-', 0:'o', 1:'x'}
     def __init__(self, size):
-        self.board = [[-1] * size] * size
-        print(self.board)
+        self.board = [[1] * size] * size
         self.size = size
 
     def __str__(self):
         N = self.size
 
-        top_border = "\t+"
-        for i in range(N):
-            top_border += "-" * N + "+"
-        print(top_border)
-
+        Board.print_border(N)
         for i in range(N):
             print("\t|", end="")
             for j in range(N):
-                print(self.VALUE_MAP[self.board[i][j]] + " | ", end="")
-            print("\n\t+---+---+---+")
+                row = " " * (N // 2) + Board.VALUE_MAP[self.board[i][j]] + " " * (N // 2) + "|" 
+                print(row, end='')
+            Board.print_border(N)
+        return ""
+
+    @staticmethod
+    def print_border(N):
+        # Allow 'x', 'o' to appear in the center of each box.
+        multiplier = N if N % 2 == 1 else N + 1
+        border = "\n\t+"
+        for i in range(N):
+            border += "-" * multiplier + "+" 
+        print(border)
+
+    def clear(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                self.board[i][j] = -1
+
+    def set_value(self, pos_x, pos_y, val):
+        # Ensure coordinates are in range.
+        assert pos_x >= 0 and pos_x <= self.size - 1
+        assert pos_y >= 0 and pos_y <= self.size - 1
+
+        self.board[pos_x][pos_y] = val
+
+    def is_full(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                if self.board[i][j] == -1:
+                    return False
+        return True
+        
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from board import Board
-
 b = Board(5)
 print(b)
+print(b.is_full())
+b.set_value(1,2,-1)
+print(b.is_full())
